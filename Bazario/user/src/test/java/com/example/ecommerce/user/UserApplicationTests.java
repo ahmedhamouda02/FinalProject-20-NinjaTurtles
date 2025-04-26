a123c9ff7007b8f584b6e757953a8b334907abb8
ahmedhamouda02 <ahmedhamouda776@gmail.com>
2025-04-26 18:53:25 +0300

    Pushing each initializr for microservice

---
+++ Bazario/user/src/test/java/com/example/ecommerce/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
