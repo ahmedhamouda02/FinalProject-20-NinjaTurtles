042bb1f280c4fb1215281ca04ce83222dd415ef4
zeinaelmofty <127438173+zeinaelmofty@users.noreply.github.com>
2025-05-17 14:25:41 +0300

    Update and rename PaymentgpApplication.java to PaymentApplication.java

--- Bazario/payment/src/main/java/com/example/payment/PaymentApplication.java
+++ Bazario/payment/src/main/java/com/example/payment/PaymentApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentgpApplication {
+public class PaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentgpApplication.class, args);
