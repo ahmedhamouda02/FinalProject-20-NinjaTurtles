f266449c26d1cafa1901eb7814713cd0e6f14b1e
gerahany <gerahany82@gmail.com>
2025-05-18 16:28:18 +0300

    order gateway

--- Bazario/order/src/main/java/com/example/ecommerce/order/OrderApplication.java
+++ Bazario/order/src/main/java/com/example/ecommerce/order/OrderApplication.java
@@ -2,6 +2,8 @@ package com.example.ecommerce.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class OrderApplication {
@@ -9,5 +11,8 @@ public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
-
+	@Bean
+	public RestClient.Builder restClientBuilder() {
+		return RestClient.builder();
+	}
 }
