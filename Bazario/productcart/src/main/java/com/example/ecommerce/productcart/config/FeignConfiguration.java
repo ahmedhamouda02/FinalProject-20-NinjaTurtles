7c7c340ce53e8e938de9345a1e75ebd19db63ba4
Ahmed Yasser Al-Taweel <96630906+ahmedyasser07@users.noreply.github.com>
2025-05-19 07:02:39 +0300

    quick fix to application.yml file -> productcart

--- Bazario/productcart/src/main/java/com/example/ecommerce/productcart/config/FeignConfiguration.java
+++ Bazario/productcart/src/main/java/com/example/ecommerce/productcart/config/FeignConfiguration.java
@@ -8,7 +8,12 @@ import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class FeignConfiguration {
+    @Bean
+    public HttpMessageConverters messageConverters() {
+        return new HttpMessageConverters(new MappingJackson2HttpMessageConverter());
+    }
 }
